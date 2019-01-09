64cba775372297a2db637ee005c75e082c266264
Earthii <xiao_eric@hotmail.com>
2019-01-09 18:12:23 -0500

    Db for challenge

---
+++ routes/data.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  keywords = req.query['keywords']
+  if(keywords){
+    // return those with keywords
+    res.json( db.get('data').filter(o => {
+      return o['keywords'].includes(keywords);
+    }).value());
+  }else{
+    // return all
+    res.json( db.get('data').value());
+  }
+});
+
+module.exports = router;
